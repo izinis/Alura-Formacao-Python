84de85e23d2b3bf1ae53d35fbbee02843c381e62
Izinis <52981360+izinis@users.noreply.github.com>
2021-08-02 21:47:49 -0300

    Edição no jogo da forca

--- Formação Python Alura/Jogos/forca.py
+++ Formação Python Alura/Jogos/forca.py
@@ -1,27 +1,53 @@
+import random
 
 def jogar():
     print("********************************")
     print("Bem vindo ao jogo de Forca")
     print("********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+    if(acertou):
+        print("Você Ganhou!😊")
+    else:
+        print("Você perdeu 😩")
+
 
     print("Fim do jogo")
 
